8ae6cb0538e50c3dcce8a4d3fead37c4c452b278
yangwenxuan233 <1282398184@qq.com>
2021-08-25 10:50:58 +0800

    day3

--- interface/interface01/demo.py
+++ interface/interface01/demo.py
@@ -1,6 +1,7 @@
 import requests
 import re
 import datetime
+# from bs4 import BeautifulSoup
 
 
 url = 'http://flash.weather.com.cn/wmaps/xml/china.xml'
@@ -8,6 +9,10 @@ header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/5
 response = requests.get(url, headers=header)
 code = response.apparent_encoding
 response.encoding = code
+html = response.content.decode(code)
+# soup = BeautifulSoup(html, 'html.parser')
+# list = soup.find_all()
+# print(list)
 html = response.text
 rows = html.split(">")
 list = []
@@ -15,6 +20,6 @@ for i in rows:
     row = re.findall(r"['\"](.*?)['\"]", i)
     if len(row) > 1:
         list.append(row)
-
+print(list)
 now_time = datetime.date.today()
 print(str(now_time))
---
+++ trainning/auto_4399.py
@@ -0,0 +1,15 @@
+import pyautogui
+import time
+
+pyautogui.FAILSAFE = True
+
+time.sleep(3)
+for count in range(500):
+    region = (567, 556, 404, 40)
+    im = pyautogui.screenshot(region=region)
+    im.save('test.png')
+    for i in range(50, 404, 100):
+        px = im.getpixel((i, 20))
+        print(px)
+        if px[0] == 1:
+            pyautogui.click(region[0] + i, region[1] + 20)
